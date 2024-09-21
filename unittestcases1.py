9f1e07ecfa848da9fa4d7025d857fd30888f3320
supriyamettela <supriyamettela8309@gmail.com>
2024-09-21 12:13:40 +0530

    Update unittestcases1.py

--- unittestcases1.py
+++ unittestcases1.py
@@ -6,14 +6,18 @@ class multiplication(unittest.testcase1):
     
     result =multiplication(3,400)
     self.asserEqual(result,1200)
+    
   def test_2(self):
 
     result =multiplication(-3,4)
     self.asserEqual(result,-12)
+    
   def test_3(self):
 
     result =multiplication(-3,-4)
     self.asserEqual(result,12)
+
+
 
 if __name__=='__main__':
   unittest.main()

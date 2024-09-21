8b9696eb96b2563d57383cc33aeeb6a9bd623b3f
supriyamettela <supriyamettela8309@gmail.com>
2024-09-21 12:06:58 +0530

    Update testcases1.py

--- unittestcases1.py
+++ unittestcases1.py
@@ -3,7 +3,7 @@ from multiply import multiplication
 class multiplication(unittest.testcase1):
 
   def test_1(self):
-
+    
     result =multiplication(3,400)
     self.asserEqual(result,1200)
   def test_2(self):

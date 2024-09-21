b5ae22411b0df63baca58c78832263ddbd87b093
supriyamettela <supriyamettela8309@gmail.com>
2024-09-21 12:18:41 +0530

    Update unittestcases1.py

--- unittestcases1.py
+++ unittestcases1.py
@@ -1,22 +1,13 @@
 import unittest
 from multiply import multiplication
-class multiplication(unittest.testcase1):
+class multiplication(unittest.TestCase):
 
   def test_1(self):
-    
+       
     result =multiplication(3,400)
     self.asserEqual(result,1200)
     
-  def test_2(self):
-
-    result =multiplication(-3,4)
-    self.asserEqual(result,-12)
-    
-  def test_3(self):
-
-    result =multiplication(-3,-4)
-    self.asserEqual(result,12)
-
+ 
 
 
 if __name__=='__main__':
